b982747ffc4d6e0e899e044dfb8d55221ed8dd6d
Kasra Khalili <Kasra85.k@outlook.com>
2024-08-05 13:25:44 +0330

    Change style and add some feature

--- src/app.js
+++ src/app.js
@@ -1,97 +1,104 @@
 const app = () => {
-  const worldWidth = 120
-  const worldHeight = 80
-  const interval = 50
-  let isPlaying = false
+  const worldWidth = 100;
+  const worldHeight = 50;
+  let interval = 50;
+  let isPlaying = false;
 
-  const worldNode = document.getElementById('world')
-  const playPauseButton = document.getElementById('play-pause')
-  const resetButton = document.getElementById('reset')
+  const worldNode = document.getElementById("world");
+  const playPauseButton = document.getElementById("play-pause");
+  const resetButton = document.getElementById("reset");
+  const setIntervalBtn = document.getElementById("setInterval");
+  const newInterval = document.getElementById("newInterval");
 
-  let worldMap = createWorldMap()
-  createWorld()
+  let worldMap = createWorldMap();
+  createWorld();
   // glider
-  worldMap[1][1] = true
-  worldMap[1][3] = true
-  worldMap[2][2] = true
-  worldMap[2][3] = true
-  worldMap[3][2] = true
+  worldMap[1][1] = true;
+  worldMap[1][3] = true;
+  worldMap[2][2] = true;
+  worldMap[2][3] = true;
+  worldMap[3][2] = true;
 
-  renderWorld()
-  bindListeners()
+  renderWorld();
+  bindListeners();
 
-  function createWorldMap() {
-    const world = []
-    for (let i = 0; i < worldHeight; i++) {
-      world[i] = []
-      for (let j = 0; j < worldWidth; j++) {
-        world[i][j] = false
+  setIntervalBtn.addEventListener("click", () => {
+    setNewInterval();
+  });
+  document.addEventListener("keydown", () => {
+    if (event.keyCode == 13) {
+      setNewInterval();
+    }
+  });
+
+  function setNewInterval() {
+    if (newInterval.value.length > 0) {
+      interval = parseInt(newInterval.value);
+      if (isPlaying) {
+        togglePlay();
+        setTimeout(togglePlay(), 1);
       }
     }
+  }
 
-    return world
+  function createWorldMap() {
+    const world = [];
+    for (let i = 0; i < worldHeight; i++) {
+      world[i] = [];
+      for (let j = 0; j < worldWidth; j++) {
+        world[i][j] = false;
+      }
+    }
+    return world;
   }
 
   function createWorld() {
     const result = worldMap.forEach((row, i) => {
-      const rowNode = document.createElement('div')
-      rowNode.className = 'row'
+      const rowNode = document.createElement("div");
+      rowNode.className = "row";
 
-      worldNode.appendChild(rowNode)
+      worldNode.appendChild(rowNode);
 
       row.forEach((_cell, j) => {
-        const cellNode = document.createElement('div')
-        cellNode.className = 'cell'
-        cellNode.id = getCellId(i, j)
+        const cellNode = document.createElement("div");
+        cellNode.className = "cell";
+        cellNode.id = getCellId(i, j);
 
-        rowNode.appendChild(cellNode)
-      })
-    })
+        rowNode.appendChild(cellNode);
+      });
+    });
   }
 
   function renderWorld() {
     worldMap.forEach((row, i) => {
       row.forEach((isAlive, j) => {
-        const cellNode = document.querySelector(`.cell#${getCellId(i, j)}`)
+        const cellNode = document.querySelector(`.cell#${getCellId(i, j)}`);
 
         if (isAlive) {
-          cellNode.classList.add('live')
+          cellNode.classList.add("live");
         } else {
-          cellNode.classList.remove('live')
+          cellNode.classList.remove("live");
         }
-      })
-    })
+      });
+    });
   }
 
   function calculateNextStep() {
-    /* const newWorldMap = createWorldMap()
-    worldMap.forEach((row, i) => {
-      row.forEach((isAlive, j) => {
-        const liveNeighborsCount = getLiveNeighborsCount(i, j)
-        newWorldMap[i][j] = willSurvive(isAlive, liveNeighborsCount)
-      })
-    }) */
-
-    return worldMap
-      .map((row, i) =>
-        row.map((isAlive, j) =>
-          willSurvive(isAlive, getLiveNeighborsCount(i, j))
-        )
-      )
-
-    // return newWorldMap
+    return worldMap.map((row, i) =>
+      row.map((isAlive, j) => willSurvive(isAlive, getLiveNeighborsCount(i, j)))
+    );
   }
 
   function getLiveNeighborsCount(i, j) {
-    const neighbors = []
+    const neighbors = [];
     for (let x = i - 1; x <= i + 1; x++) {
-      let row = worldMap[(worldHeight + x) % worldHeight]
+      let row = worldMap[(worldHeight + x) % worldHeight];
 
       for (let y = j - 1; y <= j + 1; y++) {
-        if (x == i && y == j) continue
-        let cell = row[(worldWidth + y) % worldWidth]
+        if (x == i && y == j) continue;
+        let cell = row[(worldWidth + y) % worldWidth];
 
-        neighbors.push(cell)
+        neighbors.push(cell);
       }
     }
 
@@ -101,56 +108,52 @@ const app = () => {
   function willSurvive(isAlive, liveNeighborsCount) {
     // Any live cell with fewer than two live neighbors dies, as if by under-population.
     // Any live cell with more than three live neighbors dies, as if by overpopulation.
-    if (liveNeighborsCount < 2 || liveNeighborsCount > 3)
-      return false
+    if (liveNeighborsCount < 2 || liveNeighborsCount > 3) return false;
     // Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
-    if (liveNeighborsCount == 3)
-      return true
+    if (liveNeighborsCount == 3) return true;
     // Any live cell with two or three live neighbors lives on to the next generation.
-    if (liveNeighborsCount == 2)
-      return isAlive
+    if (liveNeighborsCount == 2) return isAlive;
   }
 
   function next() {
-    worldMap = calculateNextStep()
-    renderWorld()
+    worldMap = calculateNextStep();
+    renderWorld();
   }
 
   function bindListeners() {
-    playPauseButton.addEventListener('click', togglePlay)
+    playPauseButton.addEventListener("click", togglePlay);
 
-    worldNode.addEventListener('click', (e) => {
-      const [i, j] = getCoordsFromId(e.target.id)
-      worldMap[i][j] = !worldMap[i][j]
-      renderWorld()
-    })
+    worldNode.addEventListener("click", (e) => {
+      const [i, j] = getCoordsFromId(e.target.id);
+      worldMap[i][j] = !worldMap[i][j];
+      renderWorld();
+    });
 
-    resetButton.addEventListener('click', () => {
-      if (isPlaying) togglePlay()
-      worldMap = createWorldMap()
-      renderWorld()
-    })
+    resetButton.addEventListener("click", () => {
+      if (isPlaying) togglePlay();
+      worldMap = createWorldMap();
+      renderWorld();
+    });
   }
 
   function togglePlay() {
     if (togglePlay.intervalId) {
       clearInterval(togglePlay.intervalId);
-      togglePlay.intervalId = null
-      isPlaying = false
-    }
-    else {
-      togglePlay.intervalId = setInterval(next, interval)
-      isPlaying = true
+      togglePlay.intervalId = null;
+      isPlaying = false;
+    } else {
+      togglePlay.intervalId = setInterval(next, interval);
+      isPlaying = true;
     }
   }
 
   function getCellId(i, j) {
-    return `cell-${i}-${j}`
+    return `cell-${i}-${j}`;
   }
 
   function getCoordsFromId(id) {
-    return id.split('-').splice(1).map(Number)
+    return id.split("-").splice(1).map(Number);
   }
-}
+};
 
-app()
+app();
