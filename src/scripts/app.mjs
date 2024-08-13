44364b99add5d11a9990133bc993f6f0266bf37a
Kasra Khalili <Kasra85.k@outlook.com>
2024-08-13 10:27:32 +0330

    Adding style to play/pause button

--- src/scripts/app.mjs
+++ src/scripts/app.mjs
@@ -1,5 +1,4 @@
 // TODO: Make everything functional (intro to functional programming)
-// TODO: Auto-resize world based on window sizes
 // TODO: Provide the option to add entities (like glider, etc)
 // TODO: Persist world state
 // TODO: Use SVG as the world
@@ -9,23 +8,6 @@
 import { initControls } from './controls.mjs';
 import { World } from './world.mjs';
 
-/** @type {boolean[][]} */
-// glider
-// 20, 50
-const glider = [
-  [true, false, true],
-  [false, true, true],
-  [false, true, false],
-];
-// X O X
-// O X X
-// O X O
-// worldMap[1][1] = true;
-// worldMap[1][3] = true;
-// worldMap[2][2] = true;
-// worldMap[2][3] = true;
-// worldMap[3][2] = true;
-
 const app = {
   init: () => {
     const worlds = [];
@@ -33,15 +15,10 @@ const app = {
     const nodes = document.querySelectorAll('.world');
     nodes.forEach(node => {
 
-      const world = new World(node, 10, 10);
+      const world = new World(node, 50, 50);
       initControls(world);
       worlds.push(world);
     });
-
-    // window.world2 = new World('world2');
-    // window.world3 = new World('world3');
-    // initControls(window.world2);
-    // initControls(window.world3);
   }
 };
 
