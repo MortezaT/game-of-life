2b485023cf4864c982e1567b70dca5bc434db2ac
Kasra Khalili <kasra85.k@outlook.com>
2024-08-23 22:49:21 +0330

    Fix bug and add some features

--- src/scripts/controls.mjs
+++ src/scripts/controls.mjs
@@ -1,24 +1,50 @@
 export const initControls = (world) => {
   const playPauseButton = document.getElementById('play-pause');
+  const nextStepButton = document.getElementById('next-step');
   const resetButton = document.getElementById('reset');
-  const setIntervalButton = document.getElementById('set-interval');
+  const clearButton = document.getElementById('clear');
+  const applyChangesButton = document.getElementById('apply-changes');
   const intervalInput = document.getElementById('interval-input');
+  const widthInput = document.getElementById('width-input');
+  const heightInput = document.getElementById('hight-input');
+  const inputGroup = document.getElementById('inputs-section');
+
+  intervalInput.value = world.interval;
+  widthInput.value = world.width;
+  heightInput.value = world.height;
 
   world.addEventListener('resume', () => playPauseButton.classList.add('play-pause-active'));
   world.addEventListener('stop', () => playPauseButton.classList.remove('play-pause-active'));
-  setIntervalButton.addEventListener('click', onChangeInterval);
+  applyChangesButton.addEventListener('click', onChange);
   playPauseButton.addEventListener('click', world.toggle);
+  nextStepButton.addEventListener('click', world.nextStep);
   resetButton.addEventListener('click', world.reset);
-  intervalInput.addEventListener('keydown', (event) => {
-
+  clearButton.addEventListener('click', world.clear);
+  inputGroup.addEventListener('keydown', (event) => {
     if (event.key == 'Enter') {
-      onChangeInterval();
+      onChange();
     }
   });
 
-  function onChangeInterval() {
-    if (intervalInput.value.length > 0) {
+  document.addEventListener('keydown', (event) => {
+    if (event.key == ' ') world.toggle();
+
+    if (event.key == 'Escape') world.clear();
+
+    // if (event.ctrlKey && event.key == 'ArrowRight') {}
+
+    // if (event.ctrlKey && event.key == 'ArrowLeft' && interval > 10) {}
+  });
+
+  function onChange() {
+    if (intervalInput.value > 0) {
       world.interval = +intervalInput.value;
+    }
+    if (widthInput.value > 0) {
+      world.width = +widthInput.value;
+    }
+    if (heightInput.value > 0) {
+      world.height = +heightInput.value;
     }
   }
 };
